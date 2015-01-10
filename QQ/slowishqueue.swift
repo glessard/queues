04ae41650ebd93b153ee3bb843d3899244f41977
Guillaume Lessard <dhtnstff@gmail.com>
2015-01-10 11:57:33 -0700

    A different attempt at type-erasure in the purest Swift case. Not as good as SimpleQueue.

---
+++ QQ/slowishqueue.swift
@@ -0,0 +1,117 @@
+//
+//  queue.swift
+//  concurrency
+//
+//  Created by Guillaume Lessard on 2014-08-16.
+//  Copyright (c) 2014 Guillaume Lessard. All rights reserved.
+//
+
+import Darwin
+
+final public class SlowQueue<T>: QueueType, SequenceType, GeneratorType
+{
+  private var head: AnyObject? = nil
+  private var tail: AnyObject! = nil
+
+  private var lock = OS_SPINLOCK_INIT
+
+  public init() { }
+
+  public convenience init(_ newElement: T)
+  {
+    self.init()
+    enqueue(newElement)
+  }
+
+  public var isEmpty: Bool { return head == nil }
+
+  public var count: Int {
+    return (head == nil) ? 0 : countElements()
+  }
+
+  public func countElements() -> Int
+  {
+    // Not thread safe.
+
+    var i = 0
+    var node = head as? Node<T>
+    while let n = node
+    { // Iterate along the linked nodes while counting
+      node = n.next
+      i++
+    }
+
+    return i
+  }
+
+  public func enqueue(newElement: T)
+  {
+    let newNode = Node(newElement)
+
+    OSSpinLockLock(&lock)
+    if head == nil
+    {
+      head = newNode
+      tail = newNode
+      OSSpinLockUnlock(&lock)
+      return
+    }
+
+    if let t = tail as? Node<T>
+    {
+      t.next = newNode
+      tail = newNode
+    }
+    OSSpinLockUnlock(&lock)
+  }
+
+  public func dequeue() -> T?
+  {
+    OSSpinLockLock(&lock)
+    if let node = head as? Node<T>
+    {
+      // Promote the 2nd node to 1st
+      head = node.next
+
+      // Logical housekeeping
+      if head == nil { tail = nil }
+
+      OSSpinLockUnlock(&lock)
+      return node.elem
+    }
+
+    // queue is empty
+    OSSpinLockUnlock(&lock)
+    return nil
+  }
+
+  // Implementation of GeneratorType
+
+  public func next() -> T?
+  {
+    return dequeue()
+  }
+
+  // Implementation of SequenceType
+
+  public func generate() -> Self
+  {
+    return self
+  }
+}
+
+/**
+  A simple Node for the Queue implemented above.
+  Clearly an implementation detail.
+*/
+
+private class Node<T>
+{
+  var next: Node? = nil
+  let elem: T
+
+  init(_ e: T)
+  {
+    elem = e
+  }
+}
