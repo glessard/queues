e0e0e149156cbc05c7ae36d79a5f3ca88bd6cde8
Guillaume Lessard <dhtnstff@gmail.com>
2015-02-20 11:17:01 -0700

    A non-generic Node with generic accessors for FastOSQueue.

--- QQ/fastosqueue.swift
+++ QQ/fastosqueue.swift
@@ -24,21 +24,23 @@ final public class FastOSQueue<T>: QueueType, SequenceType, GeneratorType
   deinit
   {
     // empty the queue
-    while UnsafeMutablePointer<COpaquePointer>(head).memory != nil
+    while UnsafePointer<COpaquePointer>(head).memory != nil
     {
-      let node = UnsafeMutablePointer<LinkNode>(OSAtomicFifoDequeue(head, 0))
-      UnsafeMutablePointer<T>(node.memory.elem).destroy()
-      UnsafeMutablePointer<T>(node.memory.elem).dealloc(1)
+      let node = UnsafeMutablePointer<Node>(OSAtomicFifoDequeue(head, 0))
+      let elem = node.memory.elem() as UnsafeMutablePointer<T>
+      elem.destroy()
+      elem.dealloc(1)
       node.dealloc(1)
     }
     // release the queue head structure
     AtomicQueueRelease(head)
 
     // drain the pool
-    while UnsafeMutablePointer<COpaquePointer>(pool).memory != nil
+    while UnsafePointer<COpaquePointer>(pool).memory != nil
     {
-      let node = UnsafeMutablePointer<LinkNode>(OSAtomicDequeue(pool, 0))
-      UnsafeMutablePointer<T>(node.memory.elem).dealloc(1)
+      let node = UnsafeMutablePointer<Node>(OSAtomicDequeue(pool, 0))
+      let elem = node.memory.elem() as UnsafeMutablePointer<T>
+      elem.dealloc(1)
       node.dealloc(1)
     }
     // release the pool stack structure
@@ -46,11 +48,11 @@ final public class FastOSQueue<T>: QueueType, SequenceType, GeneratorType
   }
 
   public var isEmpty: Bool {
-    return UnsafeMutablePointer<COpaquePointer>(head).memory == nil
+    return UnsafePointer<COpaquePointer>(head).memory == nil
   }
 
   public var count: Int {
-    return (UnsafeMutablePointer<COpaquePointer>(head).memory == nil) ? 0 : countElements()
+    return (UnsafePointer<COpaquePointer>(head).memory == nil) ? 0 : countElements()
   }
 
   public func countElements() -> Int
@@ -58,7 +60,7 @@ final public class FastOSQueue<T>: QueueType, SequenceType, GeneratorType
     // Not thread safe.
 
     var i = 0
-    var node = UnsafeMutablePointer<UnsafeMutablePointer<LinkNode>>(head).memory
+    var node = UnsafePointer<UnsafeMutablePointer<Node>>(head).memory
     while node != nil
     { // Iterate along the linked nodes while counting
       node = node.memory.next
@@ -70,28 +72,28 @@ final public class FastOSQueue<T>: QueueType, SequenceType, GeneratorType
 
   public func enqueue(newElement: T)
   {
-    var node = UnsafeMutablePointer<LinkNode>(OSAtomicDequeue(pool, 0))
+    var node = UnsafeMutablePointer<Node>(OSAtomicDequeue(pool, 0))
     if node == nil
     {
-      node = UnsafeMutablePointer<LinkNode>.alloc(1)
-      node.memory.elem = COpaquePointer(UnsafeMutablePointer<T>.alloc(1))
+      node = UnsafeMutablePointer<Node>.alloc(1)
+      node.memory = Node(UnsafeMutablePointer<T>.alloc(1))
     }
     node.memory.next = nil
-    UnsafeMutablePointer<T>(node.memory.elem).initialize(newElement)
+    node.memory.elem().initialize(newElement)
 
     OSAtomicFifoEnqueue(head, node, 0)
   }
 
   public func dequeue() -> T?
   {
-    let node = UnsafeMutablePointer<LinkNode>(OSAtomicFifoDequeue(head, 0))
+    let node = UnsafeMutablePointer<Node>(OSAtomicFifoDequeue(head, 0))
+
     if node != nil
     {
-      let element = UnsafeMutablePointer<T>(node.memory.elem).move()
+      let element: T = node.memory.elem().move()
       OSAtomicEnqueue(pool, node, 0)
       return element
     }
-
     return nil
   }
 
@@ -103,5 +105,21 @@ final public class FastOSQueue<T>: QueueType, SequenceType, GeneratorType
   public func generate() -> Self
   {
     return self
+  }
+}
+
+private struct Node
+{
+  var next: UnsafeMutablePointer<Node> = nil
+  var p: COpaquePointer
+
+  init<T>(_ p: UnsafeMutablePointer<T>)
+  {
+    self.p = COpaquePointer(p)
+  }
+
+  func elem<T>() -> UnsafeMutablePointer<T>
+  {
+    return UnsafeMutablePointer<T>(p)
   }
 }
