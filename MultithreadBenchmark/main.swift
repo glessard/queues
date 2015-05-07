b534b351e4d3939e8bd3af123698e958d2448524
Guillaume Lessard <dhtnstff@gmail.com>
2015-05-06 20:49:07 -0600

    A multithreaded benchmark.

---
+++ MultithreadBenchmark/main.swift
@@ -0,0 +1,61 @@
+//
+//  main.swift
+//  MultithreadBenchmark
+//
+//  Created by Guillaume Lessard on 2015-05-06.
+//  Copyright (c) 2015 Guillaume Lessard. All rights reserved.
+//
+
+let iterations: Int32 = 10_000_000
+let workers = [1,2,4,7,11,19,31]
+let maximumRun = 100
+
+//println("SlowQueue:")
+//for w in workers
+//{
+//  SlowQueueRunTest(w)
+//}
+
+//println("FastQueue:")
+//for w in workers
+//{
+//  FastQueueRunTest(w)
+//}
+//println("FastQueue with runs:")
+//for w in workers
+//{
+//  FastQueueRunTest(w, run: maximumRun)
+//}
+
+//println("Double-Lock FastQueue:")
+//for w in workers
+//{
+//  DoubleLockQueueRunTest(w)
+//}
+//println("Double-Lock FastQueue with runs:")
+//for w in workers
+//{
+//  DoubleLockQueueRunTest(w, run: maximumRun)
+//}
+
+println("Lock-Free FastQueue:")
+for w in workers
+{
+  LockFreeQueueRunTest(w)
+}
+//println("Lock-Free FastQueue with runs:")
+//for w in workers
+//{
+//  LockFreeQueueRunTest(w, run: maximumRun)
+//}
+
+println("Optimistic FastQueue:")
+for w in workers
+{
+  OptimisticQueueRunTest(w)
+}
+//println("Optimistic FastQueue with runs:")
+//for w in workers
+//{
+//  OptimisticQueueRunTest(w, run: maximumRun)
+//}
---
+++ MultithreadBenchmark/mtbench-slowqueue.swift
@@ -0,0 +1,50 @@
+//
+//  File.swift
+//  QQ
+//
+//  Created by Guillaume Lessard on 2015-05-06.
+//  Copyright (c) 2015 Guillaume Lessard. All rights reserved.
+//
+
+import Dispatch
+
+private func getQueue<T>(initialValue: T) -> SlowQueue<T>
+{
+  return SlowQueue(initialValue)
+}
+
+func SlowQueueRunTest(workers: Int, run: Int = 1)
+{
+  if workers < 1 || run < 1 { return }
+
+  var i: Int32 = 0
+
+  let queue = getQueue(arc4random())
+
+  let start = mach_absolute_time()
+  dispatch_apply(workers, dispatch_get_global_queue(qos_class_self(), 0)) {
+    _ in
+    while i < iterations
+    {
+      if (random() & 1) == 0
+      {
+        for _ in 0...arc4random_uniform(numericCast(run+1))
+        {
+          queue.enqueue(arc4random())
+        }
+      }
+      else
+      {
+        for _ in 0...arc4random_uniform(numericCast(run+1))
+        {
+          if let v = queue.dequeue()
+          {
+            OSAtomicIncrement32Barrier(&i)
+          }
+        }
+      }
+    }
+  }
+  let dt = mach_absolute_time() - start
+  println("\(workers):\t\(dt/numericCast(i))")
+}
