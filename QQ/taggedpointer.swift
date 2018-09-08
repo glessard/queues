c54fd5a96f35bd5c6f431a480360b56bb1acb596
Guillaume Lessard <dhtnstff@gmail.com>
2018-09-08 14:21:59 -0600

    rename a type parameter

--- QQ/taggedpointer.swift
+++ QQ/taggedpointer.swift
@@ -11,7 +11,7 @@ import CAtomics
 /// UInt64 as tagged pointer, as a strategy to overcome the ABA problem in
 /// synchronization algorithms based on atomic compare-and-swap operations.
 
-struct AtomicTP<T: OSAtomicNode>
+struct AtomicTP<Node: OSAtomicNode>
 {
   private var atom = AtomicUInt64()
 
@@ -21,7 +21,7 @@ struct AtomicTP<T: OSAtomicNode>
   }
 
   @inline(__always)
-  mutating func store(_ p: TaggedPointer<T>)
+  mutating func store(_ p: TaggedPointer<Node>)
   {
     atom.store(p.int, .sequential)
   }
@@ -33,14 +33,14 @@ struct AtomicTP<T: OSAtomicNode>
   }
 
   @inline(__always)
-  mutating func load() -> TaggedPointer<T>?
+  mutating func load() -> TaggedPointer<Node>?
   {
     let value = atom.load(.sequential)
     return TaggedPointer(rawValue: value)
   }
 
   @inline(__always)
-  mutating func CAS(old: TaggedPointer<T>?, new: T) -> Bool
+  mutating func CAS(old: TaggedPointer<Node>?, new: Node) -> Bool
   {
     if let old = old
     {
@@ -49,13 +49,13 @@ struct AtomicTP<T: OSAtomicNode>
     }
     else
     {
-      let new = TaggedPointer(new, tag: 1)
+      let new = TaggedPointer(new)
       return atom.CAS(0, new.int, .strong, .sequential)
     }
   }
 }
 
-struct TaggedPointer<T: OSAtomicNode>: Equatable
+struct TaggedPointer<Node: OSAtomicNode>: Equatable
 {
   private var value: UInt64
 
@@ -67,22 +67,22 @@ struct TaggedPointer<T: OSAtomicNode>: Equatable
     value = rawValue
   }
 
-  init(_ node: T)
+  init(_ node: Node)
   {
     self.init(node, tag: 1)
   }
 
-  init(_ node: T, usingTag other: TaggedPointer)
+  init(_ node: Node, usingTag other: TaggedPointer)
   {
     self.init(node, tag: other.tag)
   }
 
-  init(_ node: T, incrementingTag old: TaggedPointer)
+  init(_ node: Node, incrementingTag old: TaggedPointer)
   {
     self.init(node, tag: old.tag&+1)
   }
 
-  init(_ node: T, tag: UInt64)
+  init(_ node: Node, tag: UInt64)
   {
     #if arch(x86_64) || arch(arm64)
       value = UInt64(UInt(bitPattern: node.storage)) + (tag & 0x7fff) << 48
@@ -99,8 +99,8 @@ struct TaggedPointer<T: OSAtomicNode>: Equatable
     #endif
   }
 
-  var pointee: T {
-    return T(storage: self.pointer)
+  var pointee: Node {
+    return Node(storage: self.pointer)
   }
 
   var tag: UInt64 {
