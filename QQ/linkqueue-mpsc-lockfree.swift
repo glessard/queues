daba50d00d696e26f3f4df1f6694dfcdc33d7358
Guillaume Lessard <dhtnstff@gmail.com>
2018-10-27 14:15:29 -0600

    annotations and clarifications

--- QQ/linkqueue-mpsc-lockfree.swift
+++ QQ/linkqueue-mpsc-lockfree.swift
@@ -1,21 +1,25 @@
 //
-//  linkqueue-lockfree.swift
+//  linkqueue-mpsc-lockfree.swift
 //  QQ
 //
-//  Created by Guillaume Lessard on 2014-08-16.
-//  Copyright (c) 2014 Guillaume Lessard. All rights reserved.
+//  Created by Guillaume Lessard
+//  Copyright (c) 2018 Guillaume Lessard. All rights reserved.
 //
 
-/// Lock-free queue
+/// Multiple-Producer, Single-Consumer, Lock-Free Queue
 ///
-/// Note that this algorithm is not designed for tri-state memory as used in Swift.
-/// This means that it does not work correctly in multi-threaded situations (as in, accesses memory in an incorrect state.)
-/// It was an interesting experiment.
-///
-/// Lock-free queue algorithm adapted from Maged M. Michael and Michael L. Scott.,
+/// Adapted from Dmitry Vyukov's non-intrusive MPSC queue,
+/// http://www.1024cores.net/home/lock-free-algorithms/queues/non-intrusive-mpsc-node-based-queue
+/// which is itself a variant of a concurrent queue proposed by John M. Mellor-Crummey in
+/// https://www.cs.rice.edu/~johnmc/papers/cqueues-mellor-crummey-TR229-1987-abstract.html
+/// with the addition of Michael & Scott's dummy node insight from
 /// "Simple, Fast, and Practical Non-Blocking and Blocking Concurrent Queue Algorithms",
 /// in Principles of Distributed Computing '96 (PODC96)
-/// See also: http://www.cs.rochester.edu/research/synchronization/pseudocode/queues.html
+/// (see also: http://www.cs.rochester.edu/research/synchronization/pseudocode/queues.html)
+///
+/// This algorithm is wait-free for the Producer path, and lock-free for the Consumer path.
+/// Calls to enqueue() can be made from multiple simultaneous threads (multiple producers.)
+/// Calls to dequeue() must be serialized in some way (single consumer.)
 
 import CAtomics
 
@@ -23,28 +27,28 @@ final public class MPSCLinkQueue<T>: QueueType
 {
   public typealias Element = T
 
-  private var head = AtomicMutableRawPointer()
-  private var tail = AtomicMutableRawPointer()
+  private var head = AtomicNonNullMutableRawPointer()
+  private var tail = AtomicNonNullMutableRawPointer()
 
   public init()
-  {
-    let node = WaitFreeNode<T>()
-    head.initialize(node)
-    tail.initialize(node)
+  { // set up an initial dummy node
+    let node = Node<T>()
+    head.initialize(node: node)
+    tail.initialize(node: node)
   }
 
   deinit {
     // empty the queue
-    var node: WaitFreeNode<T>
+    var node: Node<T>
     while true
     {
-      node = head.getNode(order: .acquire)
+      node = head.loadNode(order: .acquire)
       defer { node.deallocate() }
 
-      guard let next = node.getNextNode(order: .acquire)
+      guard let next = node.loadNextNode(order: .acquire)
         else { break }
 
-      head.setNode(next, order: .release)
+      head.store(node: next, order: .release)
     }
   }
 
@@ -52,12 +56,15 @@ final public class MPSCLinkQueue<T>: QueueType
 
   public var count: Int {
     var i = 0
-    let tail: WaitFreeNode<T>  = self.tail.getNode(order: .acquire)
-    var next: WaitFreeNode<T>? = self.head.getNode(order: .acquire)
+    // only count as far as the current tail
+    let tail: Node<T>  = self.tail.loadNode(order: .acquire)
+    var next: Node<T>? = self.head.loadNode(order: .acquire)
     while let node = next, node != tail
     { // Iterate along the linked nodes while counting
       repeat {
-        next = node.getNextNode(order: .acquire)
+        // if node was not yet tail, then a `nil` next pointer
+        // does not mean we are done counting.
+        next = node.loadNextNode(order: .acquire)
       } while next == nil
       i += 1
     }
@@ -66,93 +73,118 @@ final public class MPSCLinkQueue<T>: QueueType
 
   public func enqueue(_ newElement: T)
   {
-    let node = WaitFreeNode(newElement)
+    let node = Node(newElement)
 
-    let previousTail = self.tail.swap(node: node)
-    previousTail.setNextNode(node)
+    // simultaneous producers synchronize with each other here
+    let previousTail = self.tail.swap(node: node, order: .acqrel)
+
+    /**
+     If a producer thread is interrupted here (between the swap above and the store below),
+     then the consumer can find itself encountering a head node which does not now its
+     next node, even though the queue is not empty. This prevents the consumer side of
+     the algorithm from being wait-free.
+    */
+
+    // publish the new node to the consumer here
+    previousTail.storeNextNode(node, order: .release)
   }
 
   public func dequeue() -> T?
-  {
-    let head: WaitFreeNode<T>  = self.head.getNode(order: .acquire)
-    var next: WaitFreeNode<T>? = head.getNextNode(order: .acquire)
-    if let next = next
-    {
-      let element = next.read()
-      next.clear()
-      self.head.setNode(next)
-      head.deallocate()
-      return element
-    }
-    else if tail.getNode(order: .acquire) != head
-    {
-      repeat { /* waiting */
-        next = WaitFreeNode<T>(head.next.pointee.load(.acquire))
-      } while next == nil
-      let next = next! // grumble
+  { // read the head (dummy) node and try to read the first real node
+    let head: Node<T>  = self.head.loadNode(order: .acquire)
+    var next: Node<T>? = head.loadNextNode(order: .acquire)
 
-      let element = next.read()
-      next.clear()
-      self.head.setNode(next)
-      head.deallocate()
-      return element
+    if next == nil
+    { // check whether the queue is actually empty
+      if tail.loadNode(order: .relaxed) == head
+      { // the queue is empty
+        return nil
+      }
+
+      // the queue isn't empty, but a producer hasn't finished enqueuing yet.
+      var c: UInt8 = 0
+      repeat { // wait for next to arrive
+        c = c&+1
+        if (c&0xe0) != 0
+        { // we've spun enough; time to sleep.
+          sched_yield()
+        }
+
+        next = head.loadNextNode(order: .acquire)
+      } while next == nil
     }
-    return nil
+
+    guard let node = next else { fatalError(#function) }
+
+    // get the element and clear its storage in the node
+    let element = node.move()
+    // node is now a dummy node and points ot the first real node;
+    // make self.head point to it for the next call to dequeue()
+    self.head.store(node: node, order: .release)
+    // we can now dispose of the previous head node
+    head.deallocate()
+    return element
   }
 }
 
-extension AtomicMutableRawPointer
+// Extensions used to deal with the queue's head and tail pointers
+
+extension AtomicNonNullMutableRawPointer
 {
-  mutating fileprivate func initialize<T>(_ node: WaitFreeNode<T>)
+  mutating fileprivate func initialize<T>(node: Node<T>)
   {
     self.initialize(node.storage)
   }
 
-  mutating fileprivate func getNode<T>(order: LoadMemoryOrder) -> WaitFreeNode<T>
+  mutating fileprivate func loadNode<T>(order: LoadMemoryOrder) -> Node<T>
   {
-    return WaitFreeNode(self.load(order))!
+    return Node(storage: self.load(order))
   }
 
-  mutating fileprivate func setNode<T>(_ node: WaitFreeNode<T>, order: StoreMemoryOrder = .release)
+  mutating fileprivate func store<T>(node: Node<T>, order: StoreMemoryOrder = .release)
   {
     self.store(node.storage, order)
   }
 
-  mutating fileprivate func swap<T>(node: WaitFreeNode<T>, order: MemoryOrder = .acqrel) -> WaitFreeNode<T>
+  mutating fileprivate func swap<T>(node: Node<T>, order: MemoryOrder = .acqrel) -> Node<T>
   {
-    let pointer = self.swap(node.storage, order)!
-    return WaitFreeNode(storage: pointer)
+    let pointer = self.swap(node.storage, order)
+    return Node(storage: pointer)
   }
 }
 
-private let nextOffset = MemoryLayout<UnsafeMutableRawPointer>.stride
+private let nextOffset = 0
 private let dataOffset = nextOffset + MemoryLayout<AtomicMutableRawPointer>.stride
 
-private struct WaitFreeNode<Element>: OSAtomicNode, Equatable
+private struct Node<Element>: OSAtomicNode, Equatable
 {
   let storage: UnsafeMutableRawPointer
-
-  init?(_ p: UnsafeMutableRawPointer?)
-  {
-    guard let storage = p else { return nil }
-    self.storage = storage
-  }
 
   init(storage: UnsafeMutableRawPointer)
   {
     self.storage = storage
   }
 
-  init(_ element: Element? = nil)
+  private init()
   {
     let size = dataOffset + MemoryLayout<Element?>.stride
     storage = UnsafeMutableRawPointer.allocate(byteCount: size, alignment: 16)
-    storage.bindMemory(to: (UnsafeMutableRawPointer?).self, capacity: 1)
-    storage.assumingMemoryBound(to: (UnsafeMutableRawPointer?).self).pointee = nil
     (storage+nextOffset).bindMemory(to: AtomicMutableRawPointer.self, capacity: 1)
     (storage+nextOffset).assumingMemoryBound(to: AtomicMutableRawPointer.self).pointee = AtomicMutableRawPointer()
     (storage+nextOffset).assumingMemoryBound(to: AtomicMutableRawPointer.self).pointee.initialize(nil)
-    (storage+dataOffset).bindMemory(to: (Element?).self, capacity: 1).initialize(to: element)
+    (storage+dataOffset).bindMemory(to: (Element?).self, capacity: 1)
+  }
+
+  init(none: Element? = nil)
+  {
+    self.init()
+    (storage+dataOffset).assumingMemoryBound(to: (Element?).self).initialize(to: nil)
+  }
+
+  init(_ element: Element)
+  {
+    self.init()
+    (storage+dataOffset).assumingMemoryBound(to: (Element?).self).initialize(to: element)
   }
 
   func deallocate()
@@ -161,36 +193,27 @@ private struct WaitFreeNode<Element>: OSAtomicNode, Equatable
     storage.deallocate()
   }
 
-  var next: UnsafeMutablePointer<AtomicMutableRawPointer> {
+  private var next: UnsafeMutablePointer<AtomicMutableRawPointer> {
     get {
       return (storage+nextOffset).assumingMemoryBound(to: AtomicMutableRawPointer.self)
     }
   }
 
-  func getNextNode(order: LoadMemoryOrder = .acquire) -> WaitFreeNode?
+  func loadNextNode(order: LoadMemoryOrder = .acquire) -> Node?
   {
-    return WaitFreeNode(next.pointee.load(order))
+    guard let storage = next.pointee.load(order) else { return nil }
+    return Node(storage: storage)
   }
 
-  func setNextNode(_ node: WaitFreeNode, order: StoreMemoryOrder = .release)
+  func storeNextNode(_ node: Node, order: StoreMemoryOrder = .release)
   {
     next.pointee.store(node.storage, order)
   }
 
-  func initialize(to element: Element)
+  func move() -> Element?
   {
-    storage.assumingMemoryBound(to: (UnsafeMutableRawPointer?).self).pointee = nil
-    (storage+nextOffset).assumingMemoryBound(to: AtomicMutableRawPointer.self).pointee.initialize(nil)
+    let element = (storage+dataOffset).assumingMemoryBound(to: (Element?).self).pointee
     (storage+dataOffset).assumingMemoryBound(to: (Element?).self).pointee = nil
-  }
-
-  func read() -> Element?
-  {
-    return (storage+dataOffset).assumingMemoryBound(to: (Element?).self).pointee
-  }
-
-  func clear()
-  {
-    (storage+dataOffset).assumingMemoryBound(to: (Element?).self).pointee = nil
+    return element
   }
 }
